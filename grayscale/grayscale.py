5e7695e2d6b0e26b867bbf5c68a9a548305863e2
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2023-10-17 09:12:39 +0900

    first commit

---
+++ grayscale/grayscale.py
@@ -0,0 +1,37 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def display_grayscale_image_and_histogram(input_path):
+    # 画像を開く
+    img = Image.open(input_path)
+    
+    # グレースケールに変換
+    grayscale_img = img.convert("L")
+    
+    # 画像のデータをnumpy配列に変換
+    img_data = np.asarray(grayscale_img)
+
+    # グレースケール画像を表示
+    plt.figure(figsize=(10,5))
+
+    # 左側にグレースケール画像を表示
+    plt.subplot(1, 2, 1)
+    plt.imshow(grayscale_img, cmap='gray')
+    plt.axis('off')  # 軸を非表示にする
+    plt.title('Grayscale Image')
+
+    # 右側にヒストグラムを表示
+    plt.subplot(1, 2, 2)
+    plt.hist(img_data.ravel(), bins=256, range=(0,256), density=True, color='gray', alpha=0.7)
+    plt.xlabel('Pixel Value')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Grayscale Image')
+    plt.grid(axis='y', linestyle='--')
+
+    plt.tight_layout()  # レイアウトをきれいにする
+    plt.show()
+
+# 使用例
+input_file_path = "path_to_your_image.jpg"
+display_grayscale_image_and_histogram(input_file_path)
